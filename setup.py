95002e0ec2a3b38c3d3ab955f49824b21cd9ef4f
Lukas Deutz <5903157+lagerfeuer@users.noreply.github.com>
2023-10-12 19:19:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md', encoding="utf-8") as f:
 
 setup(
     name='giig',
-    version='0.3.1',
+    version='0.3.2',
     description='CLI for gitignore.io',
     long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/lagerfeuer/gitignore',
     author='Lukas Deutz',
-    author_email='lukas.deutz@mailfence.com',
+    author_email='github@deutz.io',
     packages=['giig'],
     scripts=['bin/giig'],
     install_requires=['requests'],
